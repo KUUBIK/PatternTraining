455cf9b772152c82779277f490a075076ca56053
Robert <Robert@DESKTOP-PCFCJQN>
2021-09-24 19:30:53 +0600

    Добавьте файлы проекта.

---
+++ PatternTraining/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternTraining
+{
+    interface IStrategy
+    {
+        public int SomeData { get; set; }
+
+        void Algorithm();
+
+        virtual string Test()
+        {
+            return "1";
+        }
+    }
+}
---
+++ PatternTraining/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+
+
+namespace PatternTraining
+{
+    public delegate int Method(int x, int y);
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+        
+
+      
+    }
+
+
+    static class Test
+    {
+       public static void LeghtString(this String str)
+        {
+            Console.WriteLine($"leght to {str}:" + str.Length);
+
+
+        }
+
+
+    }
+
+
+    class Car
+    {
+        public Car() { }
+        public string Color { get; set; }
+        public string Name { get; set; }
+
+
+
+        public Car(string name, string color):this()
+        {
+            Name = name;
+            Color = color;
+        }
+    }
+
+    
+    delegate void meth(object sender, EventArgs e);
+    
+    
+    class CarEvent:EventArgs
+    {
+        public string Name { get; set; }
+
+        public CarEvent(string Name)
+        {
+            this.Name = Name;
+        }
+
+    }
+
+
+   
+    
+
+}
